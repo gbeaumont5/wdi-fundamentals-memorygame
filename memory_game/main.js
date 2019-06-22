dbb293417bb5cd3ef41ffc936103015031a6557a
Gary Beaumont <garyallenbeaumont@gmail.com>
2019-06-22 13:55:43 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -26,23 +26,35 @@ var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-}
+  alert("You found a match!");
+} else { 
+  alert("Sorry, try again.");
+};
+};
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage)
 checkForMatch();
 	if (cardsInPlay.length === 2) {
+		checkForMatch();
 } 
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 
 }
-flipCard(0);
-flipCard(2);
 
-console.log(cards[cardImage].rank);
-console.log(cards[suit].rank);
+
+function createBoard(){
+for (var i = 0; i < cards.length; i++) {
+   var cardElement = document.createElement('img');
+   cardElement.setAttribute('src', 'images/back.png');
+   cardElement.setAttribute('data-id', i);
+   cardElement.addEventListener('click', flipCard);
+   document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard(); 
 
