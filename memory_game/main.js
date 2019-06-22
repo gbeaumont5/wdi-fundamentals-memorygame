0caef7ddfc59a3a641dfedcdcc32c388c29f13e4
Gary Beaumont <garyallenbeaumont@gmail.com>
2019-06-22 12:20:14 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,4 +1,27 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{ 
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,9 +36,13 @@ function flipCard(cardId) {
 checkForMatch();
 	if (cardsInPlay.length === 2) {
 } 
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 
 }
 flipCard(0);
-flipCard(2)
+flipCard(2);
+
+console.log(cards[cardImage].rank);
+console.log(cards[suit].rank);
+
